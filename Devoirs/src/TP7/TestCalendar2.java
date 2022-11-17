67ad4c3ceb719765ee4a8ec556e40adab143430c
anfau <anfau@PC-ANNE>
2022-11-17 11:08:27 +0100

    TP7 Exercice 1

---
+++ Devoirs/src/TP7/TestCalendar2.java
@@ -0,0 +1,18 @@
+package TP7;
+
+import TP5.Keyboard;
+
+
+public class TestCalendar2 {
+
+	public class testCalendar2 {
+
+		public static void main(String[] args) {
+			Calendar2 c = new Calendar2();
+			c.creation();
+			Keyboard.getInt("attente");
+			c.creation();
+
+		}
+	}
+}
