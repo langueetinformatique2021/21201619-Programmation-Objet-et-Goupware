860a222df7cf015bdc03ad943b56b0f5d17ef2cc
anfau <anfau@192.168.0.51>
2022-11-24 11:49:15 +0100

    TP8

---
+++ Devoirs/src/TP8/lancerDés.java
@@ -0,0 +1,15 @@
+package TP8;
+
+public class lancerDés extends Aleat {
+	public lancerDés() {
+		super(6);
+		
+	}
+	
+	public TroisDés lancer() {
+		TroisDés tire = new TroisDés(super.get(), super.get(), super.get());
+		return tire;
+		
+	}
+
+}
